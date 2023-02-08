8db001672ea3479b9a07c96e36b602793e228197
walkingdevel <mosquito.is.dead@gmail.com>
2023-02-08 21:29:47 +0000

    wip: add tip class to blockquotes

--- src/HTMLTransformer.v
+++ src/HTMLTransformer.v
@@ -7,12 +7,17 @@ mut:
 
 fn (mut t HTMLTransformer) process() string {
 	t.add_main_class_to_first_h1()
+	t.add_tip_class_to_blockquotes()
 
 	return t.content
 }
 
 fn (mut t HTMLTransformer) add_main_class_to_first_h1() {
 	t.content = t.content.replace_once('<h1', '<h1 class="main"')
+}
+
+fn (mut t HTMLTransformer) add_tip_class_to_blockquotes() {
+	t.content = t.content.replace_once('<blockquote', '<blockquote class="tip"')
 }
 
 fn (t &HTMLTransformer) add_anchors() {
