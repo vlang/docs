51410e209ddedb25bee2391da4dba573373bc137
Delyan Angelov <delian66@gmail.com>
2024-05-06 15:35:54 +0300

    update the sass vs sassc situation on the FreeBSD vps

--- sass.v
+++ sass.v
@@ -4,10 +4,11 @@ import os
 
 fn update_sass() {
 	// Note: sass is https://sass-lang.com/dart-sass/, while sassc is https://github.com/sass/sassc .
-	// On the FreeBSD vps, where the documentation is generated, *only `sassc` is available*,
-	// and the `sass` tool, does not have an easily installable prebuilt version for FreeBSD.
+	// On the FreeBSD vps, where the documentation is generated, *only `sassc` is available* natively,
+	// and the `sass` tool, does not have an easily installable prebuilt native version for FreeBSD,
+	// only a slower transpiled to JS version, installed through `npm install -g sass`.
 	//
-	// They both produce nearly equal output with --style=expanded , except for:
+	// In comparison, they both produce nearly equal output with --style=expanded , except for:
 	// a) `transition: fill 0.25s;` vs `transition: fill .25s;`
 	// b) some newlines at the end of each CSS rule
 	// c) style.css.map is different.
